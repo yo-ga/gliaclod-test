f18b91694e7962eab9914cbb7501e67b3b44a22f
YOGA <art96962006@gmail.com>
2017-06-23 17:33:42 +0800

    Update ppt.py

--- part2/ppt.py
+++ part2/ppt.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://www.ppt.com/gossip/index.html'
+board = "Cross_Life"
+url = "https://www.ppt.cc/bbs/"+board+"/index.html"
 re=requests.get(url)
 
-soup=BeautifulSoup(re.text.encode('utf-8'), "html.parser")
+soup = BeautifulSoup(re.text.encode('utf-8'), "html.parser")
 
 for line in soup.select('.r-ent'):
-    print ('日期:', line.select('.date')[0].text)
-    print ('作者:', line.select('.author')[0].text)
-    print ('標題:', line.select('.title')[0].text)
-    print ('看板名稱:', 'Gossip')
+    print ("日期:", line.select('.date')[0].text)
+    print ("作者:", line.select('.author')[0].text)
+    print ("標題:", line.select('.title')[0].text)
+    print ("看板名稱:", board)
     print()
